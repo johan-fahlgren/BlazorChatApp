53baee1661bf12a9a8777b03ff3ea3ae083c4777
johanfahlgren <johan.fahlgren@learnet.se>
2022-06-13 21:45:37 +0200

    ProjectTemplate

---
+++ MudChat/Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudChat.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("MudChat.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("MudChat.ServerAPI"));
+
+builder.Services.AddApiAuthorization();
+
+await builder.Build().RunAsync();
+
---
+++ MudChat/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MudChat.Server.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}
+
